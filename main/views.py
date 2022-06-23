519ada5dc2fd47310043816e33afc862e1fc6349
Kristofers Solo <kristians.cagulis@gmail.com>
2022-06-23 19:12:16 +0300

    Added Lightsaber

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 def index(request):
 	return render(request, 'main/index.html', {'title': 'Homepage'})
+
+
+def lighsaber(request):
+	return render(request, 'main/lightsaber.html', {'title': 'Lightsaber'})
