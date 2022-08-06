a6dc2552790c2c1897497252a4cabf6b31d6d1c9
Kristofers Solo <kristians.cagulis@proton.me>
2022-08-06 19:24:47 +0300

    Created lightsaber page

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('lightsaber', views.lightsaber, name='lightsaber'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     """Homepage"""
     return render(request, 'main/index.html', {'title': 'Kristofers Solo Webpage'})
+
+
+def lightsaber(request):
+    """Lightsaber page"""
+    return render(request, 'main/lightsaber.html', {'title': 'Lightsaber'})
