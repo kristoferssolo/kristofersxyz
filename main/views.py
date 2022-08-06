e9ef11bc46740960f24fc992d09d646998f1583f
Kristofers Solo <kristians.cagulis@proton.me>
2022-08-06 16:17:18 +0300

    Created homepage

--- main/views.py
+++ main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def index(request):
     """Homepage"""
-    return render(request, 'main/index.html', {'title': 'Homepage'})
+    return render(request, 'main/index.html', {'title': 'Kristofers Solo Webpage'})
