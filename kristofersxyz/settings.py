057aabba158e77d5cb5bac98aaeed38f4753578c
Kristofers Solo <kristians.cagulis@proton.me>
2023-01-20 03:26:17 +0200

    Changed gif to webm

--- kristofersxyz/settings.py
+++ kristofersxyz/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = Path.joinpath(BASE_DIR, "static")
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_PATH.joinpath("static")
 MEDIA_ROOT = BASE_PATH.joinpath("media")
